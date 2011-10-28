6ef11d7238690c367b970ad91f0d7f3f4686776e
Jon Linden <jon8linden@gmail.com>
2011-10-28 14:52:36 -0400

    Added Level statistics menu screen. Got LevelSelect closer to final product.

--- src/com/se/softwareEngineering/LevelSelect.java
+++ src/com/se/softwareEngineering/LevelSelect.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class LevelSelect extends Activity{
 
@@ -14,14 +17,29 @@ public class LevelSelect extends Activity{
 		setContentView(R.layout.levelselect);
 		
 		
-		//Level 1 button
-		Button btn_Level1 = (Button) findViewById (R.id.btnlevel1);
-		btn_Level1.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				startActivity(new Intent("com.se.softwareEngineering.test2d.Test2D"));
-			}
-		});
+		//Grid layout
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new ImageAdapter(this));
+		
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            	//Goes to LevelStats need to look up statistics later dependent on level that is picked (position)
+            	if (position == 0){
+            		startActivity(new Intent("com.se.softwareEngineering.LEVELSTATS"));
+            	}
+            	else if (position ==1){
+            		Toast.makeText(LevelSelect.this, "Locked Complete Level 1",Toast.LENGTH_SHORT).show();
+            	}
+            	else if (position ==2){
+            		Toast.makeText(LevelSelect.this, "Locked Complete Level 2",Toast.LENGTH_SHORT).show();
+            	}
+            	else {
+            		Toast.makeText(LevelSelect.this, "Comming Soon",Toast.LENGTH_SHORT).show();
+            	}
+            }
+        });
+
+		
 	}
 
 }
