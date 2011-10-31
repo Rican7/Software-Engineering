bbfa5ea4c40423b8ca79a7e7c42a12467e14e309
Jon Linden <jon8linden@gmail.com>
2011-10-31 15:39:40 -0400

    forgot to add this file to last commit

--- src/com/se/softwareEngineering/LevelSelect.java
+++ src/com/se/softwareEngineering/LevelSelect.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
@@ -16,6 +17,15 @@ public class LevelSelect extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.levelselect);
 		
+		Button btn_exit = (Button) findViewById (R.id.btnBack);
+		btn_exit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+		});
 		
 		//Grid layout
 		GridView gridview = (GridView) findViewById(R.id.gridview);
